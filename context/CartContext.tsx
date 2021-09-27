f60c40a79145528b1d3724ae4c8646335a57263f
Lê Đình Hiếu <wanderlust252@gmail.com>
2021-09-27 13:19:19 +0700

    blog

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -11,18 +11,18 @@ export type DATA =  {
   quatity: number;
 }
 const contextDefaultValues: CartContextState = { 
-  data:[],
+  data:[
+    {id:0,name:'Bánh trung thu thập cẩm',img:thapcam, quatity:0},
+    {id:1,name:'Bánh trung thu đậu xanh',img:dauxanh, quatity:0},
+    {id:2,name:'Bánh trung thu Quảng Đông',img:quangdong, quatity:0},
+    {id:3,name:'Bánh trung thu sầu riêng',img:saurieng, quatity:0},
+  ],
   addQuatity: ()=>0,
 };
 export const CartContext = createContext<CartContextState>(contextDefaultValues);
   
 const CartProvider: FunctionComponent = ({ children }) => {
-  const [data,setData] = useState([
-    {id:0,name:'Bánh trung thu thập cẩm',img:thapcam, quatity:0},
-    {id:1,name:'Bánh trung thu đậu xanh',img:dauxanh, quatity:0},
-    {id:2,name:'Bánh trung thu Quảng Đông',img:quangdong, quatity:0},
-    {id:3,name:'Bánh trung thu sầu riêng',img:saurieng, quatity:0},
-  ]);
+  const [data,setData] = useState(contextDefaultValues.data);
   const addQuatity = (id:number)=>{
     const tmp: DATA[] = [...data].map((item,index)=>{
       if(item.id===id)item.quatity +=1;
